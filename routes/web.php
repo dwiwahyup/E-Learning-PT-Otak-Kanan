14f88c3cc828a33d65c0207f131042595d7a29be
MuhammadFadhil1024 <fadhilnur102001@gmail.com>
2022-09-29 12:46:33 +0700

    add middleware admin

--- routes/web.php
+++ routes/web.php
@@ -35,54 +35,57 @@ Route::get('/', function () {
 Route::get('/home', [HomeController::class, 'index']);
 
 Route::middleware(['auth', 'verified'])->prefix('dashboard')->group(function () {
-    Route::get('/', [DashboardController::class, 'index']);
 
-    // route for contents
-    Route::get('/content/{id}', [ContentController::class, 'index']);
-    Route::get('/content/create/{id}', [ContentController::class, 'create']);
-    Route::post('/content/store', [ContentController::class, 'store']);
-    Route::get('/content/edit/{id}', [ContentController::class, 'edit']);
-    Route::post('/content/update', [ContentController::class, 'update']);
-    Route::get('/content/delete/{id}', [ContentController::class, 'delete']);
+    Route::middleware(['admin'])->group(function () {
+        Route::get('/', [DashboardController::class, 'index']);
 
-    // route for logbook
-    Route::get('/logbook', [LogbookController::class, 'index']);
-    Route::get('/logbook/create', [LogbookController::class, 'create']);
-    Route::post('/logbook/store', [LogbookController::class, 'store']);
-    Route::get('/logbook/edit/{id}', [LogbookController::class, 'edit']);
-    Route::post('/logbook/update', [LogbookController::class, 'update']);
-    Route::get('/logbook/delete/{id}', [LogbookController::class, 'delete']);
+        // route for contents
+        Route::get('/content/{id}', [ContentController::class, 'index']);
+        Route::get('/content/create/{id}', [ContentController::class, 'create']);
+        Route::post('/content/store', [ContentController::class, 'store']);
+        Route::get('/content/edit/{id}', [ContentController::class, 'edit']);
+        Route::post('/content/update', [ContentController::class, 'update']);
+        Route::get('/content/delete/{id}', [ContentController::class, 'delete']);
 
-    Route::get('/class', [ClassController::class, 'index']);
-    Route::get('/class/class1', [ClassController::class, 'class1']);
-    Route::get('/class/class2', [ClassController::class, 'class2']);
-    Route::get('/class/class3', [ClassController::class, 'class3']);
-    Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
-    Route::get('/mahasiswa/create', [MahasiswaController::class, 'create']);
+        // route for logbook
+        Route::get('/logbook', [LogbookController::class, 'index']);
+        Route::get('/logbook/create', [LogbookController::class, 'create']);
+        Route::post('/logbook/store', [LogbookController::class, 'store']);
+        Route::get('/logbook/edit/{id}', [LogbookController::class, 'edit']);
+        Route::post('/logbook/update', [LogbookController::class, 'update']);
+        Route::get('/logbook/delete/{id}', [LogbookController::class, 'delete']);
 
-    // route for chapter
-    Route::get('/chapter/{id}', [ChapterController::class, 'index']);
-    Route::get('/chapter/create/{id}', [ChapterController::class, 'create']);
-    Route::post('/chapter/store', [ChapterController::class, 'store']);
-    Route::get('/chapter/edit/{id}', [ChapterController::class, 'edit']);
-    Route::post('/chapter/update', [ChapterController::class, 'update']);
-    Route::get('/chapter/delete/{id}', [ChapterController::class, 'delete']);
+        Route::get('/class', [ClassController::class, 'index']);
+        Route::get('/class/class1', [ClassController::class, 'class1']);
+        Route::get('/class/class2', [ClassController::class, 'class2']);
+        Route::get('/class/class3', [ClassController::class, 'class3']);
+        Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+        Route::get('/mahasiswa/create', [MahasiswaController::class, 'create']);
 
-    // route for course catagory
-    Route::get('/coursecategory', [CourseCategoryController::class, 'index']);
-    Route::get('/coursecategory/create', [CourseCategoryController::class, 'create']);
-    Route::post('/coursecategory/store', [CourseCategoryController::class, 'store']);
-    Route::get('/coursecategory/edit/{id}', [CourseCategoryController::class, 'edit']);
-    Route::post('/coursecategory/update', [CourseCategoryController::class, 'update']);
-    Route::get('/coursecategory/delete/{id}', [CourseCategoryController::class, 'delete']);
+        // route for chapter
+        Route::get('/chapter/{id}', [ChapterController::class, 'index']);
+        Route::get('/chapter/create/{id}', [ChapterController::class, 'create']);
+        Route::post('/chapter/store', [ChapterController::class, 'store']);
+        Route::get('/chapter/edit/{id}', [ChapterController::class, 'edit']);
+        Route::post('/chapter/update', [ChapterController::class, 'update']);
+        Route::get('/chapter/delete/{id}', [ChapterController::class, 'delete']);
 
-    //route for quiz
-    Route::get('/quiz/{id}', [QuizController::class, 'index']);
-    Route::get('/quiz/create/{id}', [QuizController::class, 'create']);
-    Route::post('/quiz/store', [QuizController::class, 'store']);
-    Route::get('/quiz/edit/{id}', [QuizController::class, 'edit']);
-    Route::post('/quiz/update', [QuizController::class, 'update']);
-    Route::get('/quiz/delete/{id}', [QuizController::class, 'delete']);
+        // route for course catagory
+        Route::get('/coursecategory', [CourseCategoryController::class, 'index']);
+        Route::get('/coursecategory/create', [CourseCategoryController::class, 'create']);
+        Route::post('/coursecategory/store', [CourseCategoryController::class, 'store']);
+        Route::get('/coursecategory/edit/{id}', [CourseCategoryController::class, 'edit']);
+        Route::post('/coursecategory/update', [CourseCategoryController::class, 'update']);
+        Route::get('/coursecategory/delete/{id}', [CourseCategoryController::class, 'delete']);
+
+        //route for quiz
+        Route::get('/quiz/{id}', [QuizController::class, 'index']);
+        Route::get('/quiz/create/{id}', [QuizController::class, 'create']);
+        Route::post('/quiz/store', [QuizController::class, 'store']);
+        Route::get('/quiz/edit/{id}', [QuizController::class, 'edit']);
+        Route::post('/quiz/update', [QuizController::class, 'update']);
+        Route::get('/quiz/delete/{id}', [QuizController::class, 'delete']);
+    });
 });
 
 require __DIR__ . '/auth.php';
