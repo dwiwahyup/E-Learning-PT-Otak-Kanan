446731964a8ccef95d79db556286498880033cff
MuhammadFadhil1024 <fadhilnur102001@gmail.com>
2022-10-05 13:28:09 +0700

    Update index.blade.php

--- resources/views/dashboard/chapter/index.blade.php
+++ resources/views/dashboard/chapter/index.blade.php
@@ -55,27 +55,27 @@
                     </div>
                 </div>
             </div>  
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Delete chapter</h5>
+                            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">×</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            Are you sure to delete this chapter ?
+                        </div>
+                        <div class="modal-footer">
+                            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                            <a class="btn btn-primary" href="/dashboard/chapter/delete/{{$data->id}}">Delete</a>
+                        </div>
+                    </form>
+                    </div>
+                </div>
+            </div>
         @endforeach
-    </div>
-</div>
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Delete chapter</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                Are you sure to delete this chapter ?
-            </div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="/dashboard/chapter/delete/{{$data->id}}">Delete</a>
-            </div>
-        </form>
-        </div>
     </div>
 </div>
 @endsection
