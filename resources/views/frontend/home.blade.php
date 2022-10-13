3b052c900d521861f4671549613be3907739f541
Dwi Wahyu Prambodo <71806064+DwiWahyuP@users.noreply.github.com>
2022-10-13 20:06:32 +0700

    Update home.blade.php

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -2,44 +2,49 @@
 
 @section('content')
 <main>
-    <section class="header-video">
-        <div id="hero_video">
-            <div class="wrapper">
-                <div class="container">
-                    <h3>Book unique experiences</h3>
-                    <p>Expolore top rated tours, hotels and restaurants around the world</p>
-                    <form>
-                        <div class="row no-gutters custom-search-input-2">
-                            <div class="col-lg-4">
-                                <div class="form-group">
-                                    <input class="form-control" type="text" placeholder="What are you looking for...">
-                                    <i class="icon_search"></i>
-                                </div>
-                            </div>
-                            <div class="col-lg-3">
-                                <div class="form-group">
-                                    <input class="form-control" type="text" placeholder="Where">
-                                    <i class="icon_pin_alt"></i>
-                                </div>
-                            </div>
-                            <div class="col-lg-3">
-                                <select class="wide">
-                                    <option>All Categories</option>
-                                    <option>Tours</option>
-                                    <option>Hotels</option>
-                                    <option>Restaurants</option>
-                                </select>
-                            </div>
-                            <div class="col-lg-2">
-                                <input type="submit" class="btn_search" value="Search">
+    <section class="hero_single version_2">
+        <div class="wrapper">
+            <div class="container">
+                <h3>Book unique experiences</h3>
+                <p>Expolore top rated tours, hotels and restaurants around the world</p>
+                <form>
+                    <div class="row no-gutters custom-search-input-2">
+                        <div class="col-lg-4">
+                            <div class="form-group">
+                                <input class="form-control" type="text" placeholder="Hotel, City...">
+                                <i class="icon_pin_alt"></i>
                             </div>
                         </div>
-                        <!-- /row -->
-                    </form>
-                </div>
+                        <div class="col-lg-3">
+                            <div class="form-group">
+                                <input class="form-control" type="text" name="dates" placeholder="When..">
+                                <i class="icon_calendar"></i>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="panel-dropdown">
+                                <a href="#">Guests <span class="qtyTotal">1</span></a>
+                                <div class="panel-dropdown-content">
+                                    <!-- Quantity Buttons -->
+                                    <div class="qtyButtons">
+                                        <label>Adults</label>
+                                        <input type="text" name="qtyInput" value="1">
+                                    </div>
+                                    <div class="qtyButtons">
+                                        <label>Childrens</label>
+                                        <input type="text" name="qtyInput" value="0">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-2">
+                            <input type="submit" class="btn_search" value="Search">
+                        </div>
+                    </div>
+                    <!-- /row -->
+                </form>
             </div>
         </div>
-        <img src="{{ url('frontend/img/video_fix.png')}}" alt="" class="header-video--media" data-video-src="video/intro" data-teaser-source="video/intro" data-provider="" data-video-width="1920" data-video-height="960">
     </section>
     <!-- /header-video -->
 
