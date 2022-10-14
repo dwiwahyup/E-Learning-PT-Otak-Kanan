1f5b7bb48421f569f46254b47e0c8774dddc5e17
A_E41190139_LigaBayuHerdianto <71804899+LigaBayuHerdianto@users.noreply.github.com>
2022-10-14 21:56:36 +0700

    update testimoni

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -347,9 +347,7 @@
     <div class="bg_color_1">
         <div class="container margin_80_55">
             <div class="main_title_2">
-                <span><em></em></span>
-                <h3>News and Events</h3>
-                <p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
+                <h3>Testimoni</h3>
             </div>
             <div class="row">
                 <div class="col-lg-6">
@@ -358,65 +356,77 @@
                             <figcaption><strong>28</strong>Dec</figcaption>
                         </figure>
                         <ul>
-                            <li>Mark Twain</li>
-                            <li>20.11.2017</li>
+                            <li>Firman Wahyudi</li>
                         </ul>
-                        <h4>Pri oportere scribentur eu</h4>
-                        <p>Cu eum alia elit, usu in eius appareat, deleniti sapientem honestatis eos ex. In ius esse ullum vidisse....</p>
+                        <div class="info">
+                                <div class="cat_star"><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i></div>
+                            
+                            </div>
+                        {{-- <h4>Pri oportere scribentur eu</h4> --}}
+                        <p align="justify">Magang di PT. Otak Kanan sangat seru dan materi yang disampaikan sangat jelas. Penyampaian materi juga diajarkan terlebih dahulu kemudian dicoba untuk prakter. Terima kasih PT. Otak Kanan.</p>
                     </a>
                 </div>
                 <!-- /box_news -->
                 <div class="col-lg-6">
                     <a class="box_news" href="#0">
-                        <figure><img src="{{ url('frontend/img/news_home_2.jpg')}}" alt="">
+                        <figure><img src="{{ url('frontend/img/news_home_1.jpg')}}" alt="">
                             <figcaption><strong>28</strong>Dec</figcaption>
                         </figure>
                         <ul>
-                            <li>Jhon Doe</li>
-                            <li>20.11.2017</li>
+                            <li>Firman Wahyudi</li>
                         </ul>
-                        <h4>Duo eius postea suscipit ad</h4>
-                        <p>Cu eum alia elit, usu in eius appareat, deleniti sapientem honestatis eos ex. In ius esse ullum vidisse....</p>
+                        <div class="info">
+                                <div class="cat_star"><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i></div>
+                            
+                            </div>
+                        {{-- <h4>Pri oportere scribentur eu</h4> --}}
+                        <p align="justify">Magang di PT. Otak Kanan sangat seru dan materi yang disampaikan sangat jelas. Penyampaian materi juga diajarkan terlebih dahulu kemudian dicoba untuk prakter. Terima kasih PT. Otak Kanan.</p>
                     </a>
                 </div>
                 <!-- /box_news -->
                 <div class="col-lg-6">
                     <a class="box_news" href="#0">
-                        <figure><img src="{{ url('frontend/img/news_home_3.jpg')}}" alt="">
+                        <figure><img src="{{ url('frontend/img/news_home_1.jpg')}}" alt="">
                             <figcaption><strong>28</strong>Dec</figcaption>
                         </figure>
                         <ul>
-                            <li>Luca Robinson</li>
-                            <li>20.11.2017</li>
+                            <li>Firman Wahyudi</li>
                         </ul>
-                        <h4>Elitr mandamus cu has</h4>
-                        <p>Cu eum alia elit, usu in eius appareat, deleniti sapientem honestatis eos ex. In ius esse ullum vidisse....</p>
+                        <div class="info">
+                                <div class="cat_star"><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i></div>
+                            
+                            </div>
+                        {{-- <h4>Pri oportere scribentur eu</h4> --}}
+                        <p align="justify">Magang di PT. Otak Kanan sangat seru dan materi yang disampaikan sangat jelas. Penyampaian materi juga diajarkan terlebih dahulu kemudian dicoba untuk prakter. Terima kasih PT. Otak Kanan.</p>
                     </a>
                 </div>
                 <!-- /box_news -->
                 <div class="col-lg-6">
                     <a class="box_news" href="#0">
-                        <figure><img src="{{ url('frontend/img/news_home_4.jpg')}}" alt="">
+                        <figure><img src="{{ url('frontend/img/news_home_1.jpg')}}" alt="">
                             <figcaption><strong>28</strong>Dec</figcaption>
                         </figure>
                         <ul>
-                            <li>Paula Rodrigez</li>
-                            <li>20.11.2017</li>
+                            <li>Firman Wahyudi</li>
                         </ul>
-                        <h4>Id est adhuc ignota delenit</h4>
-                        <p>Cu eum alia elit, usu in eius appareat, deleniti sapientem honestatis eos ex. In ius esse ullum vidisse....</p>
+                        <div class="info">
+                                <div class="cat_star"><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i><i class="icon_star"></i></div>
+                            
+                            </div>
+                        {{-- <h4>Pri oportere scribentur eu</h4> --}}
+                        <p align="justify">Magang di PT. Otak Kanan sangat seru dan materi yang disampaikan sangat jelas. Penyampaian materi juga diajarkan terlebih dahulu kemudian dicoba untuk prakter. Terima kasih PT. Otak Kanan.</p>
                     </a>
                 </div>
                 <!-- /box_news -->
             </div>
             <!-- /row -->
-            <p class="btn_home_align"><a href="blog.html" class="btn_1 rounded">View all news</a></p>
+            {{-- <p class="btn_home_align"><a href="blog.html" class="btn_1 rounded">View all news</a></p> --}}
         </div>
         <!-- /container -->
     </div>
     <!-- /bg_color_1 -->
 
-    <div class="call_section">
+    {{-- <div class="call_section">
         <div class="container clearfix">
             <div class="col-lg-5 col-md-6 float-right wow" data-wow-offset="250">
                 <div class="block-reveal">
@@ -429,7 +439,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
     <!--/call_section-->
 </main>
 <!-- /main -->
