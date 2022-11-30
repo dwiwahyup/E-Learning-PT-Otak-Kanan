d594d0d854c0b6fc2b6794a7d64edd09cb41c42c
Dwi Wahyu Prambodo <71806064+DwiWahyuP@users.noreply.github.com>
2022-11-30 19:12:38 +0700

    update navbar dashboard

--- resources/views/components/dashboard/navigation.blade.php
+++ resources/views/components/dashboard/navigation.blade.php
@@ -15,7 +15,7 @@
                 </a>
             </li>
             <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Messages">
-                <a class="nav-link" href="{{('/dashboard/mentors')}}">
+                <a class="nav-link" href="{{route('profile.index')}}">
                     <i class="fa fa-user-o" aria-hidden="true"></i>
                     <span class="nav-link-text">My Profile</span>
                 </a>
@@ -40,18 +40,9 @@
             </li>
 
             <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Messages">
-                <a class="nav-link" href="{{route('profile.index')}}">
-                    <i class="fa fa-user-o" aria-hidden="true"></i>
-                    <span class="nav-link-text">My Profile</span>
                 <a class="nav-link" href="{{('/dashboard/program')}}">
                     <i class="fa fa-user" aria-hidden="true"></i>
                     <span class="nav-link-text">Programs</span>
-                </a>
-            </li>
-            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Messages">
-                <a class="nav-link" href="{{('/dashboard/testimonial')}}">
-                    <i class="fa fa-user" aria-hidden="true"></i>
-                    <span class="nav-link-text">Testimonials</span>
                 </a>
             </li>
             <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Messages">
