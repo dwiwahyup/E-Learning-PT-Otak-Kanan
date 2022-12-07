1fbb9556e63b7b6583faea08a936ff150170e318
MuhammadFadhil1024 <61375944+MuhammadFadhil1024@users.noreply.github.com>
2022-12-07 12:09:41 +0700

    update count mentor on dashboard admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,7 +13,8 @@ class DashboardController extends Controller
         $materi = DB::table('contents')->count();
         $userr = DB::table('users')->count();
         $courses = DB::table('course_categories')->count();
-        $mentors = DB::table('mentors')->count();
+        $mentors = User::where('roles', 'MENTOR')->count();
+        // dd($mentors);
         return view('dashboard.index', compact('materi', 'userr', 'courses', 'mentors'));
     }
 }
